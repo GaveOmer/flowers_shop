7ddaf8d210e732e7fb40da9e32d787ff882c05f9
feitan <ilya.shkoda06@gmail.com>
2024-01-27 16:09:09 +0300

    api settings and swagger-ui

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include 
-from drf_spectacular.views import SpectacularSwaggerView 
+from drf_spectacular.views import SpectacularAPIView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('schema/', SpectacularSwaggerView.as_view(), name='schema'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
