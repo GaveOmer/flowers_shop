69b0a6456042a9ebc5d7d4924abd3dd21fe523ff
feitan <ilya.shkoda06@gmail.com>
2024-01-30 00:07:04 +0300

    vite vue setup

---
+++ xfrontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Index from './components/Index.vue'
+</script>
+
+<template>
+  <div>
+    <Index/>
+    <div class="container">
+      <p>sdsdsdsds</p>
+      <p>sdsdsdsds</p>
+      <p>sdsdsdsds</p>
+      <p>sdsdsdsds</p>
+    </div>
+  </div>
+  
+  
+</template>
+
+<style scoped>
+
+</style>
