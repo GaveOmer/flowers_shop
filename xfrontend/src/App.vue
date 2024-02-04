3f5e1c1c455683db36c121e6c830118343ff10ba
feitan <ilya.shkoda06@gmail.com>
2024-02-04 14:24:20 +0300

    vue start

--- xfrontend/src/App.vue
+++ xfrontend/src/App.vue
@@ -1,19 +1,24 @@
-<script setup>
+<script>
 import Index from './components/Index.vue'
 import HeaderApp from './components/HeaderApp.vue'
+import ChangeColorButton from './components/ChangeColorButton'
+export default {
+	components: {
+		ChangeColorButton,
+		Index,
+		HeaderApp
+	},
+}
 </script>
 
 <template>
-  <div>
-    <HeaderApp/>
-    <div class="">
-      <Index/>
-    </div>
-  </div>
-  
-  
+	<div>
+		<HeaderApp />
+		<div class="flex flex-col items-center justify-center h-screen">
+			<Index />
+			<ChangeColorButton class="" />
+		</div>
+	</div>
 </template>
 
-<style scoped>
 
-</style>
--- xfrontend/src/components/Index.vue
+++ xfrontend/src/components/Index.vue
@@ -1,46 +1,45 @@
-<script setup></script>
-
 <template>
 	<div>
-    <h1>Загруженные данные</h1>
-    <div v-if="loading">Загрузка...</div>
-    <div v-else-if="error">{{ error }}</div>
-    <div v-else>
-      <ul class="">
-        <li v-for="item in data" :key="item.slug">
-          {{ item.name }}
-          <img :src="imageUrl" alt="Описание изображения">
-        </li>
-      </ul>
-      <pre>{{ data }}</pre>
-    </div>
-  </div>
+		<h1>Загруженные данные</h1>
+		<div v-if="loading">Загрузка...</div>
+		<div v-else-if="error">{{ error }}</div>
+		<div v-else>
+			<ul class="">
+				<li v-for="item in data" :key="item.slug">
+					{{ item.name }}
+					{{ item.description }}
+					{{ item.price }}
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
-  data() {
-    return {
-      data: null,
-      loading: true,
-      error: null,
-    };
-  },
-  mounted() {
-    axios.get('http://127.0.0.1:8000/api/products/')
-      .then(response => {
-        this.data = response.data;
-      })
-      .catch(error => {
-        console.error("Ошибка при получении данных: ", error);
-        this.error = 'Произошла ошибка при загрузке данных';
-      })
-      .finally(() => {
-        this.loading = false;
-      });
-  }
-};
+	data() {
+		return {
+			data: null,
+			loading: true,
+			error: null,
+		}
+	},
+	mounted() {
+		axios
+			.get('http://127.0.0.1:8000/api/products/')
+			.then((response) => {
+				this.data = response.data
+			})
+			.catch((error) => {
+				console.error('Ошибка при получении данных: ', error)
+				this.error = 'Произошла ошибка при загрузке данных'
+			})
+			.finally(() => {
+				this.loading = false
+			})
+	},
+}
 </script>
